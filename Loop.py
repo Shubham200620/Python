d9dccd3f02e1f6afcefc43dc64b6f3cee5b97765
Shubham <jhashubham384@gmail.com>
2025-09-14 09:02:36 +0000

    this script dipicts for and while loop in python

---
+++ Loop.py
@@ -0,0 +1,12 @@
+# For Loop
+
+for i in range(0,10):
+    print(f"checking server {i}...")
+
+# while loop
+server_up=False
+while not server_up:
+    print("Waiting for the server to start...")
+    server_up=True
+print("Server is Running and up!")
+
