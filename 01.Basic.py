a2e65d288305d164e8855791d11f96bbaebef1aa
Shubham <jhashubham384@gmail.com>
2025-09-14 08:53:30 +0000

    This script dipicts how to declare string, Integer , Float and Boolean value in Python

---
+++ 01.Basic.py
@@ -0,0 +1,20 @@
+#print Message
+print("Hello, Devops!")
+
+#String
+name="Nikunj Soni- Devops Trainer"
+print(name)
+
+#Integer
+task_completed=10
+print(task_completed)   
+
+#Float
+cpu_usage = 78.5
+print(cpu_usage)
+
+#Boolean
+is_active = True
+print(is_active)
+
+
